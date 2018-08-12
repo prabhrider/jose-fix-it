96c532ede2daf94c978bba0a905dc191b9083fdf
Prabhjot Singh <prabhrider@gmail.com>
2018-08-12 16:17:31 -0500

    Add all services info

--- js/main.js
+++ js/main.js
@@ -46,6 +46,17 @@
     $('.portfolio-modal').on('hidden.bs.modal', function(e) {
       $(".navbar").removeClass("d-none");
     })
+
+    // Floating label headings for the contact form
+  $(function() {
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+      $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+      $(this).removeClass("floating-label-form-group-with-focus");
+    });
+  });
   
   })(jQuery); // End of use strict
   
