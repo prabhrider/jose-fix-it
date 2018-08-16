98a41bb403c642af5f7ee110af78c092e9c82906
Prabhjot Singh <prabhrider@gmail.com>
2018-08-16 00:47:05 -0500

    Change contact me form suitable for mobile

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 (function($) {
     "use strict"; // Start of use strict
-  
+
+    $('textarea#message').characterCounter({
+      counterSelector: '.character-counter',
+      increaseCounting: true,
+      renderTotal: true      
+    });
+
+    $('textarea#message').focus(function() {
+      $(this).next("span").show();
+    });
+    $('textarea#message').focusout(function() {
+      $(this).next("span").hide();
+    });
+
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
